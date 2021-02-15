0732a4b389ecb8253a05a774e95c026f066df74a
Kristina Al-ashqar <kristinaal-ashqar@Kristinas-MacBook-Air.local>
2021-02-15 17:14:15 -0600

    index handlebars route is working

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,8 +1,17 @@
 const model = require("../models/burger.js")
+const express = require('express')
+const router = express.Router()
 
-app.get('/burgers', (req, res)=>{
+
+router.get('/api/burgers', (req, res)=>{
     model.selectAll(function (data) {
         res.json(data)
         res.render('index' , data)
     })
 })
+
+router.get('/', (req , res)=>{
+    res.render('index')
+})
+
+module.exports = router;
